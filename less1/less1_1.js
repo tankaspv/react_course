3d538fe35bb6c5c44289b6de858ca18910f6ca4c
Tatiana Grishkina <t.grishkina@drweb.com>
2019-03-28 17:41:27 +0300

    added new folder for Less1

---
+++ less1/less1_1.js
@@ -0,0 +1,19 @@
+// Написать функцию loop, которая будет принимать параметры: times (значение по умолчанию = 0), callback (значение по умолчанию = null)
+// и будет в цикле (times раз), вызывать функцию callback. Если функцию не передана, то цикл не должен отрабатывать ни разу.
+// Покажите применение этой функции.
+
+function loop(times = 0, callback = null) {
+    if (typeof callback == 'function') {
+      for (let i = 0; i < times; i++) {
+        callback();
+      }
+    };
+    return true;
+  }
+  
+  function hello() {
+    console.log('Hello');
+}
+  
+console.log(loop(10, hello));
+console.log(loop(10, 8));
